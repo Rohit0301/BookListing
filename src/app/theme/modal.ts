90f7242311fb8063e962201f1385e4149e430412
Rohit Jain <rohitjain@Rohits-MacBook-Air.local>
2024-10-30 15:32:52 +0530

    minor: remove unnecessary css from modal theme config

--- src/app/theme/modal.ts
+++ src/app/theme/modal.ts
@@ -2,37 +2,15 @@ export const modalStyles = {
   MuiModal: {
     styleOverrides: {
       root: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
         "& .custom-modal": {
           position: "absolute",
           top: "50%",
           left: "50%",
           width: "600px",
-          backgroundColor: "#fff",
           padding: "2px",
           borderRadius: "4px",
+          backgroundColor: "#fff",
           transform: "translate(-50%, -50%)",
-        },
-        "& .modal-header": {
-          display: "flex",
-          padding: "8px",
-          alignItems: "center",
-          justifyContent: "space-between",
-          borderBottom: "1px solid #cecece",
-        },
-        "& .modal-header .title": {
-          fontSize: "20px",
-          fontWeight: 500,
-        },
-        "& .modal-footer": {
-          gap: "12px",
-          padding: "8px",
-          display: "flex",
-          justifyContent: "end",
-          alignItems: "center",
-          borderTop: "1px solid #cecece",
         },
       },
     },
